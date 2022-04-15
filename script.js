2a85741dd57025d78c7c831ad5b8fbcda042f715
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-04-15 07:52:17 +0000

    fix linter errors

--- script.js
+++ script.js
@@ -1,123 +1,123 @@
-const navMenu = document.querySelector(".mobile-list");
-const navItems = document.querySelectorAll(".nav-items");
-const closeNav = document.querySelector(".close-nav");
-const humburger = document.querySelector(".union");
+const navMenu = document.querySelector('.mobile-list');
+const navItems = document.querySelectorAll('.nav-items');
+const closeNav = document.querySelector('.close-nav');
+const humburger = document.querySelector('.union');
 function menuToggler() {
-  if (navMenu.classList.contains("displayMenu")) {
-    navMenu.classList.remove("displayMenu");
-    humburger.style.display = "block";
-    closeNav.style.display = "none";
-    navMenu.style.display = "none";
-    document.body.style.overflow = "auto";
+  if (navMenu.classList.contains('displayMenu')) {
+    navMenu.classList.remove('displayMenu');
+    humburger.style.display = 'block';
+    closeNav.style.display = 'none';
+    navMenu.style.display = 'none';
+    document.body.style.overflow = 'auto';
   } else {
-    navMenu.classList.add("displayMenu");
-    humburger.style.display = "none";
-    closeNav.style.display = "block";
-    navMenu.style.display = "block";
-    document.body.style.overflow = "hidden";
+    navMenu.classList.add('displayMenu');
+    humburger.style.display = 'none';
+    closeNav.style.display = 'block';
+    navMenu.style.display = 'block';
+    document.body.style.overflow = 'hidden';
   }
 }
-humburger.addEventListener("click", menuToggler);
-closeNav.addEventListener("click", menuToggler);
+humburger.addEventListener('click', menuToggler);
+closeNav.addEventListener('click', menuToggler);
 navItems.forEach((navItem) => {
-  navItem.addEventListener("click", menuToggler);
+  navItem.addEventListener('click', menuToggler);
 });
 
 const dynamicProjects = [
   {
-    name: "Multi-Post Stories Gain+Glory",
-    technologies: ["Ruby on rails", "JavaScript", "html"],
+    name: 'Multi-Post Stories Gain+Glory',
+    technologies: ['Ruby on rails', 'JavaScript', 'html'],
     desktopTechnologiesPopup: [
-      "Codekit",
-      "Github",
-      "JavaScript",
-      "Terminal",
-      "Codepen",
+      'Codekit',
+      'Github',
+      'JavaScript',
+      'Terminal',
+      'Codepen',
     ],
-    mobileTechnologiesPopup: ["Ruby on rails", "CSS", "JavaScript"],
-    shortDescription: "Keeping track of hundreds of components",
+    mobileTechnologiesPopup: ['Ruby on rails', 'CSS', 'JavaScript'],
+    shortDescription: 'Keeping track of hundreds of components',
     longDescription:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    desktopPopupImage: "./images/SnapshootPopUpDesktop.svg",
-    mobilePopupImage: "./images/SnaptshotPopUpMobile.png",
-    liveSource: "",
-    seeSource: "",
+    desktopPopupImage: './images/SnapshootPopUpDesktop.svg',
+    mobilePopupImage: './images/SnaptshotPopUpMobile.png',
+    liveSource: '',
+    seeSource: '',
   },
   {
-    name: "Multi-Post Stories Gain+Glory",
-    technologies: ["Ruby on rails", "JavaScript", "html"],
+    name: 'Multi-Post Stories Gain+Glory',
+    technologies: ['Ruby on rails', 'JavaScript', 'html'],
     desktopTechnologiesPopup: [
-      "Codekit",
-      "Github",
-      "JavaScript",
-      "Terminal",
-      "Codepen",
+      'Codekit',
+      'Github',
+      'JavaScript',
+      'Terminal',
+      'Codepen',
     ],
-    mobileTechnologiesPopup: ["Ruby on rails", "CSS", "JavaScript"],
-    shortDescription: "Keeping track of hundreds of components",
+    mobileTechnologiesPopup: ['Ruby on rails', 'CSS', 'JavaScript'],
+    shortDescription: 'Keeping track of hundreds of components',
     longDescription:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    desktopPopupImage: ".images/SnapshootPopUpDesktop.svg",
-    mobilePopupImage: "./images/SnaptshotPopUpMobile.png",
-    liveSource: "",
-    seeSource: "",
+    desktopPopupImage: '.images/SnapshootPopUpDesktop.svg',
+    mobilePopupImage: './images/SnaptshotPopUpMobile.png',
+    liveSource: '',
+    seeSource: '',
   },
   {
-    name: "Multi-Post Stories Gain+Glory",
-    technologies: ["Ruby on rails", "JavaScript", "html"],
+    name: 'Multi-Post Stories Gain+Glory',
+    technologies: ['Ruby on rails', 'JavaScript', 'html'],
     desktopTechnologiesPopup: [
-      "Codekit",
-      "Github",
-      "JavaScript",
-      "Terminal",
-      "Codepen",
+      'Codekit',
+      'Github',
+      'JavaScript',
+      'Terminal',
+      'Codepen',
     ],
-    mobileTechnologiesPopup: ["Ruby on rails", "CSS", "JavaScript"],
-    shortDescription: "Keeping track of hundreds of components",
+    mobileTechnologiesPopup: ['Ruby on rails', 'CSS', 'JavaScript'],
+    shortDescription: 'Keeping track of hundreds of components',
     longDescription:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    desktopPopupImage: ".images/SnapshootPopUpDesktop.svg",
-    mobilePopupImage: "./images/SnaptshotPopUpMobile.png",
-    liveSource: "",
-    seeSource: "",
+    desktopPopupImage: '.images/SnapshootPopUpDesktop.svg',
+    mobilePopupImage: './images/SnaptshotPopUpMobile.png',
+    liveSource: '',
+    seeSource: '',
   },
   {
-    name: "Multi-Post Stories Gain+Glory",
-    technologies: ["Ruby on rails", "JavaScript", "html"],
+    name: 'Multi-Post Stories Gain+Glory',
+    technologies: ['Ruby on rails', 'JavaScript', 'html'],
     desktopTechnologiesPopup: [
-      "Codekit",
-      "Github",
-      "JavaScript",
-      "Terminal",
-      "Codepen",
+      'Codekit',
+      'Github',
+      'JavaScript',
+      'Terminal',
+      'Codepen',
     ],
-    mobileTechnologiesPopup: ["Ruby on rails", "CSS", "JavaScript"],
-    shortDescription: "Keeping track of hundreds of components",
+    mobileTechnologiesPopup: ['Ruby on rails', 'CSS', 'JavaScript'],
+    shortDescription: 'Keeping track of hundreds of components',
     longDescription:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    desktopPopupImage: ".images/SnapshootPopUpDesktop.svg",
-    mobilePopupImage: "./images/SnaptshotPopUpMobile.png",
-    liveSource: "",
-    seeSource: "",
+    desktopPopupImage: '.images/SnapshootPopUpDesktop.svg',
+    mobilePopupImage: './images/SnaptshotPopUpMobile.png',
+    liveSource: '',
+    seeSource: '',
   },
   {
-    name: "Multi-Post Stories Gain+Glory",
-    technologies: ["Ruby on rails", "JavaScript", "html"],
+    name: 'Multi-Post Stories Gain+Glory',
+    technologies: ['Ruby on rails', 'JavaScript', 'html'],
     desktopTechnologiesPopup: [
-      "Codekit",
-      "Github",
-      "JavaScript",
-      "Terminal",
-      "Codepen",
+      'Codekit',
+      'Github',
+      'JavaScript',
+      'Terminal',
+      'Codepen',
     ],
-    mobileTechnologiesPopup: ["Ruby on rails", "CSS", "JavaScript"],
-    shortDescription: "Keeping track of hundreds of components",
+    mobileTechnologiesPopup: ['Ruby on rails', 'CSS', 'JavaScript'],
+    shortDescription: 'Keeping track of hundreds of components',
     longDescription:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    desktopPopupImage: ".images/SnapshootPopUpDesktop.svg",
-    mobilePopupImage: "./images/SnaptshotPopUpMobile.png",
-    liveSource: "",
-    seeSource: "",
+    desktopPopupImage: '.images/SnapshootPopUpDesktop.svg',
+    mobilePopupImage: './images/SnaptshotPopUpMobile.png',
+    liveSource: '',
+    seeSource: '',
   },
 ];
 
@@ -126,11 +126,11 @@ function getElement(className) {
 }
 
 function technologyList(technologies) {
-  const langList = document.createElement("ul");
-  langList.classList.add("recent-work__items-languages");
+  const langList = document.createElement('ul');
+  langList.classList.add('recent-work__items-languages');
   for (let i = 0; i < technologies.length; i += 1) {
-    const langElement = document.createElement("li");
-    langElement.classList.add("lang");
+    const langElement = document.createElement('li');
+    langElement.classList.add('lang');
     langList.appendChild(langElement);
     langElement.innerHTML = technologies[i];
   }
@@ -142,24 +142,24 @@ function myWorkSection() {
   for (let i = 0; i < dynamicProjects.length; i += 1) {
     allProjects = dynamicProjects[i];
     const { technologies } = allProjects;
-    const workSection = document.createElement("template");
+    const workSection = document.createElement('template');
     workSection.innerHTML = `<article class="article-card"> 
     <img src="./images/Imgplaholder.svg" alt="Image placeholder"> <h2 class="recent-work__items">
     ${allProjects.name}
     </h2> <ul class="recent-work__items-languages">${technologyList(
-      technologies
-    )}</ul> <button type="button" class="see-projects open-modal" id="see-project">See Projects</button> </article>`;
-    getElement(".recent-work").appendChild(workSection.content.firstChild);
+    technologies,
+  )}</ul> <button type="button" class="see-projects open-modal" id="see-project">See Projects</button> </article>`;
+    getElement('.recent-work').appendChild(workSection.content.firstChild);
   }
 }
 
 let projectspopup;
 
 function popUp() {
-  const mq = window.matchMedia("(min-width: 67.5rem)");
+  const mq = window.matchMedia('(min-width: 67.5rem)');
   if (mq.matches) {
     // window width is at  1080 px or more
-    const desktop = document.querySelector(".desktop-popcontainer");
+    const desktop = document.querySelector('.desktop-popcontainer');
     desktop.innerHTML = `
     <div class="desktop-popclass" id="desktop-popid">
     <div class="desktop-popimages">
@@ -204,13 +204,13 @@ function popUp() {
   </div>
     `;
 
-    document.body.style.overflow = "hidden";
-    desktop.style.display = "flex";
+    document.body.style.overflow = 'hidden';
+    desktop.style.display = 'flex';
   } else {
     // window width is less than 1080px
-    const mobile = document.querySelector(".mobile-popcontainer");
-    document.body.style.overflow = "hidden";
-    mobile.style.display = "flex";
+    const mobile = document.querySelector('.mobile-popcontainer');
+    document.body.style.overflow = 'hidden';
+    mobile.style.display = 'flex';
     mobile.innerHTML = `
     <div class="mobile-popclass" id="mobile-popid">
     <div class="pop-imagesbtn">
@@ -251,23 +251,23 @@ function popUp() {
 }
 
 function getButtons() {
-  projectspopup = document.getElementsByClassName("open-modal");
+  projectspopup = document.getElementsByClassName('open-modal');
   projectspopup = Array.from(projectspopup);
-  projectspopup.forEach((e) => e.addEventListener("click", popUp));
+  projectspopup.forEach((e) => e.addEventListener('click', popUp));
 }
 
-const closeMobilePopup = document.querySelector(".desktop-popcontainer");
-const closeDesktopPopup = document.querySelector(".mobile-popcontainer");
-const closeMobPopup = document.querySelectorAll(".exit-button");
+const closeMobilePopup = document.querySelector('.desktop-popcontainer');
+const closeDesktopPopup = document.querySelector('.mobile-popcontainer');
+const closeMobPopup = document.querySelectorAll('.exit-button');
 
 function closeButton() {
-  closeMobilePopup.style.display = "none";
-  closeDesktopPopup.style.display = "none";
-  document.body.style.overflow = "hidden";
+  closeMobilePopup.style.display = 'none';
+  closeDesktopPopup.style.display = 'none';
+  document.body.style.overflow = 'hidden';
 }
 
-closeMobPopup.forEach((a) => a.addEventListener("click", closeButton));
-window.addEventListener("click", (e) => {
+closeMobPopup.forEach((a) => a.addEventListener('click', closeButton));
+window.addEventListener('click', (e) => {
   if (e.target === closeDesktopPopup || e.target === closeMobilePopup) {
     closeButton();
   }
